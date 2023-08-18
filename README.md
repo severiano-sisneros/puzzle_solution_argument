3dc023a00555503c0a1bc0622395b5ba89ba340c
Severiano Sisneros <seve.sisneros@protonmail.com>
2023-08-18 01:05:05 -0700

    Changed protocol to use ECDSA for proof generation. Mitigating front-runner attack.

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
 This is a simple protocol for arguing the knowledge of a solution to a puzzle. Solutions are expected to take the form of an ordered set of strings.
 
-The following protocol is adapted from the $\Sigma$-protocol from *[Proofs, Arguments, and Zero Knowledge](https://people.cs.georgetown.edu/jthaler/ProofsArgsAndZK.pdf)* (Chapter 12, Protocol 3).
+Treat $w$ as a private key used for signing. Then, the solver signs a message which contains their public address. The awards go to the address in the signed message.
+
+For compatibility with Ethereum, we could simply use ECDSA with secp256k1.
 
 **Setup**
-1. Let $G$ be a (multiplicative) cyclic group of prime order with generator $g$
-2. Commitment key is initialized with $g$
+1. Let $G$ be a (multiplicative) cyclic group of prime order with generator $g$ derived from the $SECP256k1$ elliptic curve
+2. CrosswordCourt is initialized with $g$ 
+
+*In practice this is already handled by the EVM by default, since we will be using the pre-compiled contracts for signature verification.*
 
 **Author Posting Puzzle**
 1. Author computes puzzle witness $w = puzzleHash(puzzleSolution)$
-2. Author computes puzzle commitment $h = g^w$, where only the Author knows $w$
+2. Author posts puzzle commitment $h = g^w$ to the CrosswordCourt. 
+
+*Effectively, $w$ is the private signing key and $h$ is the public key*
 
 **Player Proving Solution**
 1. Player computes $w' = puzzleHash(puzzleGuess)$
-2. Player checks that they know the solution by checking that $g^{w'} = h$ 
-3. Player picks a random number $r$ in ${0,...,|G|−1}$ and computes $a ← g^r$
-4. Player computes Fiat-Shamir challenge $e = hash(g, h, a)$
-5. Player computes $z ← (w'e+r) mod |G|$.
-6. Player submits $(a, z)$ to CrosswordCourt.
+2. Player checks that they know the solution by checking that $g^{w'} = h$, i.e. $w' = w$ with high probability.
+3. Player computes $a = ECDSA\_sign_w(m_p)$, where $m_p$ is the player's public address.
+6. Player submits $(a, m_p)$ to CrosswordCourt.
 
-**Verifying Solution**
-1. Compute Fiat-Shamir challenge $e = hash(g, h, a)$, 
-2. Check that $a · h^e = g^z$.
+**Crossword Court Verifying Solution**
+1. Check $ECDSA\_verify_h(a, m_p) == true$
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,29 @@
 
-use ark_ec::{Group, CurveGroup, AffineRepr};
-use ark_ff::{PrimeField, UniformRand};
-use ark_serialize::{SerializationError, CanonicalSerialize};
+
+use ethers_core::k256::ecdsa::{SigningKey, VerifyingKey, Signature, signature::{Signer, Verifier}};
 use digest::Digest;
-use rand::{rngs::StdRng, SeedableRng};
+use anyhow::Result;
 
 /// PuzzleSolution is a struct that contains a set of `solutions` to a puzzle and the key `g` used to commit to them.
 /// The solutions are expected as strings.
-pub struct PuzzleSolution<G: Group + CurveGroup> {
+pub struct PuzzleSolution {
     solutions: Vec<String>,
-    g: G,
 }
 
 /// PuzzleSolutionProof is a struct that contains a proof that a given solution is a valid solution to a puzzle.
 /// The proof is essentially a Schnorr proof of knowledge of the discrete logarithm of a commitment to the solution.
-pub struct PuzzleSolutionProof<G: Group + CurveGroup> {
-    a: G,
-    z: G::ScalarField,
+pub struct PuzzleSolutionProof {
+    a: Signature,
+    m_s: Vec<u8>,
 }
 
-impl<G: Group + CurveGroup> PuzzleSolution<G> {
-    pub fn new(solutions: Vec<String>, g: G) -> Self {
-        Self { solutions, g }
+impl PuzzleSolution{
+    pub fn new(solutions: Vec<String>) -> Self {
+        Self { solutions}
     }
 
     /// get_solution_commitment returns the commitment to the solution set.
-    pub fn get_solution_commitment<H: Digest>(&self) -> (G::ScalarField, G) {
+    pub fn get_solution_commitment<H: Digest>(&self) -> Result<(SigningKey, VerifyingKey)>  {
         // Compute chain of hashes of each solution in the solution set
         // and commit to the last hash in the chain
         let mut hasher = H::new();
@@ -35,143 +33,68 @@ impl<G: Group + CurveGroup> PuzzleSolution<G> {
         }
 
         let w_bytes = hasher.finalize();
-        let w = G::ScalarField::from_be_bytes_mod_order(&w_bytes);
-        (w, self.g.mul(w))
+
+        let w = SigningKey::from_slice(&w_bytes)?;
+        let binding = w.clone();
+        let h = binding.verifying_key();
+        Ok((w, *h))
     }
 
     /// get_solution_proof returns a proof that the given solution is a valid solution to the puzzle.
-    pub fn get_solution_proof<H: Digest>(
+    pub fn get_solution_proof(
         &self,
-        w: G::ScalarField,
-        solution_commitment: G,
-    ) -> Result<PuzzleSolutionProof<G>, SerializationError> {
+        w_bytes: Vec<u8>,
+        solution_commitment: VerifyingKey,
+        m_s: Vec<u8>,
+    ) -> Result<PuzzleSolutionProof> {
         // Check that w is correct solution
         //TODO: Better error handling
-        match self.g.mul(w) == solution_commitment {
+        let w = SigningKey::from_slice(&w_bytes)?;
+        let h = w.verifying_key();
+        match *h == solution_commitment {
             true => (),
-            false => return Err(SerializationError::InvalidData),
+            false => return Err(anyhow::Error::msg("Invalid solution")),
         }
 
-        // Draw a random blinding number, r
-        let mut rng = StdRng::from_entropy();
-        let r = G::ScalarField::rand(&mut rng);
-
         // Send blinding commitment
-        let a = self.g.mul(r);
+        let a = w.sign(m_s.as_slice());
 
-        // Compute Fiat-Shamir challenge, e
-        let e = compute_fiat_shamir_challenge::<G, H>(vec![self.g, solution_commitment, a])?;
-
-        // Compute challenge response, z
-        let z = w * e + r;
-
-        Ok(PuzzleSolutionProof { a, z })
+        Ok(PuzzleSolutionProof{ a, m_s})
     }
 }
 
-impl<G: Group + CurveGroup> PuzzleSolutionProof<G> {
+impl PuzzleSolutionProof {
     /// verify returns true if the proof is valid and false otherwise.
     pub fn verify<H: Digest>(
         &self,
-        g: G,
-        solution_commitment: G,
-    ) -> Result<bool, SerializationError> {
-        // Compute Fiat-Shamir challenge, e
-        let e = compute_fiat_shamir_challenge::<G, H>(vec![g, solution_commitment, self.a])?;
+        solution_commitment: VerifyingKey,
+    ) -> Result<bool> {
 
         // Verify proof
-        let lhs = self.a + solution_commitment.mul(&e);
-        let rhs = g.mul(self.z);
-        Ok(lhs == rhs)
+        let result = solution_commitment.verify(self.m_s.as_slice(), &self.a);
+        Ok(result.is_ok())
     }
 
     /// verify returns serialized bytes for self.a, self.z, g, and solution_commitment if the proof is valid and error otherwise.
-    pub fn verify_and_export<H: Digest>(
+    pub fn verify_and_export(
         &self,
-        g: G,
-        solution_commitment: G,
-    ) -> Result<(Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>, Vec<u8>), SerializationError> {
-        // Compute Fiat-Shamir challenge, e
-        let e = compute_fiat_shamir_challenge::<G, H>(vec![g, solution_commitment, self.a])?;
-        let a = self.a;
-        let z = self.z;
-
+        solution_commitment: VerifyingKey,
+    ) -> Result<(Vec<u8>, Vec<u8>   )> {
+  
         // Verify proof
-        let lhs = a + solution_commitment.mul(&e);
-        let rhs = g.mul(&z);
-        match lhs == rhs {
+        let result = solution_commitment.verify(&self.m_s.as_slice(), &self.a);
+        match result.is_ok() {
             true => {
-                let (ax_bytes, ay_bytes) = serialize_point(a)?;
-                let (gx_bytes, gy_bytes) = serialize_point(g)?;
-                let (solution_commitment_x_bytes, solution_commitment_y_bytes) =
-                    serialize_point(solution_commitment)?;
-                let z_bytes = serialize_scalar::<G>(z)?;
+                let a_bytes = self.a.to_vec();
 
                 Ok((
-                    ax_bytes,
-                    ay_bytes,
-                    gx_bytes,
-                    gy_bytes,
-                    solution_commitment_x_bytes,
-                    solution_commitment_y_bytes,
-                    z_bytes
+                    a_bytes,
+                    self.m_s.clone()
                 ))
             }
-            false => Err(SerializationError::InvalidData),
+            false => Err(anyhow::Error::msg("Invalid proof")),
         }
     }
-}
-
-fn serialize_scalar<G: Group >(input_element: G::ScalarField) -> Result<Vec<u8>, SerializationError> {
-    let mut bytes = Vec::new();
-    input_element.serialize_uncompressed(&mut bytes)?;
-    Ok(bytes)
-}
-
-// This serializes a point in uncompressed format. 
-// Just running the serialize_uncompressed function on the projective point results in a y that is sometimes not reduced mod p (maybe when y is negative).
-// This causes issues with Ethereum's precompiles when checking if the point is on the curve. 
-// So the code here is to make sure that the y is reduced mod p.
-
-fn serialize_point<G: Group + CurveGroup>(p: G) -> Result<(Vec<u8>, Vec<u8>), SerializationError> {
-    let p_x = match p.into_affine().x(){
-        Some(x) => *x,
-        None => return Err(SerializationError::InvalidData),
-    };
-    
-    let p_y = match p.into_affine().y(){
-        Some(y) => *y,
-        None => return Err(SerializationError::InvalidData),
-    };
-
-    let mut x_bytes = Vec::new();
-    p_x.serialize_uncompressed(&mut x_bytes)?;
-    let mut y_bytes = Vec::new();
-    p_y.serialize_uncompressed(&mut y_bytes)?;
-    Ok((x_bytes, y_bytes))
-}
-
-fn compute_fiat_shamir_challenge<G: Group + CurveGroup, H: Digest>(input_elements: Vec<G>) -> Result<G::ScalarField, SerializationError> {
-    let mut hasher = H::new();
-    let mut element_bytes = Vec::new();
-    
-    // Ethereum expects numbers in big endian format so need to reverse each element separately
-    for element in input_elements {
-        let (x, y) = serialize_point(element)?;
-        element_bytes.extend(x.iter().rev().collect::<Vec<_>>());
-        element_bytes.extend(y.iter().rev().collect::<Vec<_>>());
-    }
-
-    hasher.update(element_bytes);
-    let hash_out = hasher.finalize();
-
-    // Mask to 254 bits, we don't want the value to be larger than the group order. Makes it easier for smart contract.
-    let mut hash_254 = vec![hash_out[0]&0x3f];
-    (1..32)
-        .into_iter()
-        .for_each(|i| hash_254.push(hash_out[i] & 0xff));
-
-    Ok(G::ScalarField::from_be_bytes_mod_order(&hash_254))
 }
 
 // Tests
@@ -179,33 +102,21 @@ fn compute_fiat_shamir_challenge<G: Group + CurveGroup, H: Digest>(input_element
 // test for get_solution_root
 mod tests {
     use super::*;
-    use ark_bn254;
     use sha2;
+    use ethers_signers::{Signer, LocalWallet};
 
     #[test]
-    fn test_get_solution_root() {
+    fn test_get_solution_commitment() {
         let solutions = vec![
             "solution1".to_string(),
             "solution2".to_string(),
             "solution3".to_string(),
         ];
-        let g = ark_bn254::G1Projective::generator();
-        let puzzle_solution = PuzzleSolution::<ark_bn254::G1Projective>::new(solutions, g);
-        let puzzle_commitment = puzzle_solution.get_solution_commitment::<sha2::Sha256>();
+        let puzzle_solution = PuzzleSolution::new(solutions);
+        let (w, puzzle_commitment) = puzzle_solution.get_solution_commitment::<sha2::Sha256>().unwrap();
 
-        let wrong_solutions = vec![
-            "Solution1".to_string(),
-            "solution2".to_string(),
-            "solution3".to_string(),
-        ];
-        let wrong_puzzle_solution =
-            PuzzleSolution::<ark_bn254::G1Projective>::new(wrong_solutions, g);
-        let wrong_puzzle_commitment =
-            wrong_puzzle_solution.get_solution_commitment::<sha2::Sha256>();
-
-        assert!(puzzle_commitment != wrong_puzzle_commitment);
+        assert!(*w.verifying_key() == puzzle_commitment);
     }
-
     // Test for get_solution_proof and verify
     #[test]
     fn test_get_solution_proof() {
@@ -214,16 +125,16 @@ mod tests {
             "solution2".to_string(),
             "solution3".to_string(),
         ];
-        let g = ark_bn254::G1Projective::generator();
-        let puzzle_solution = PuzzleSolution::<ark_bn254::G1Projective>::new(solutions, g);
-        let (w, puzzle_commitment) = puzzle_solution.get_solution_commitment::<sha2::Sha256>();
+        let puzzle_solution = PuzzleSolution::new(solutions);
+        let (w, puzzle_commitment) = puzzle_solution.get_solution_commitment::<sha2::Sha256>().unwrap();
+        let m_s = LocalWallet::new(&mut rand::thread_rng()).address().as_bytes().to_vec();
         let puzzle_solution_proof = puzzle_solution
-            .get_solution_proof::<sha2::Sha256>(w, puzzle_commitment)
+            .get_solution_proof(w.to_bytes().to_vec(), puzzle_commitment, m_s.clone())
             .unwrap();
 
         assert!(
             puzzle_solution_proof
-                .verify::<sha2::Sha256>(g, puzzle_commitment)
+                .verify::<sha2::Sha256>(puzzle_commitment)
                 .unwrap()
                 == true
         );
@@ -234,80 +145,44 @@ mod tests {
             "solution3".to_string(),
         ];
         let wrong_puzzle_solution =
-            PuzzleSolution::<ark_bn254::G1Projective>::new(wrong_solutions, g);
-        let (w, wrong_puzzle_commitment) =
-            wrong_puzzle_solution.get_solution_commitment::<sha2::Sha256>();
+            PuzzleSolution::new(wrong_solutions);
+        let (wrong_w, wrong_puzzle_commitment) =
+            wrong_puzzle_solution.get_solution_commitment::<sha2::Sha256>().unwrap();
         let wrong_puzzle_solution_proof = wrong_puzzle_solution
-            .get_solution_proof::<sha2::Sha256>(w, wrong_puzzle_commitment)
+            .get_solution_proof(wrong_w.to_bytes().to_vec(), wrong_puzzle_commitment, m_s)
             .unwrap();
 
         assert!(
             wrong_puzzle_solution_proof
-                .verify::<sha2::Sha256>(g, puzzle_commitment)
+                .verify::<sha2::Sha256>(puzzle_commitment)
                 .unwrap()
                 == false
         );
     }
 
-    // check that the get_solution_proof returns error if solution is wrong
-    #[test]
-    fn test_get_solution_proof_error() {
-        let solutions = vec![
-            "solution1".to_string(),
-            "solution2".to_string(),
-            "solution3".to_string(),
-        ];
-        let g = ark_bn254::G1Projective::generator();
-        let puzzle_solution = PuzzleSolution::<ark_bn254::G1Projective>::new(solutions, g);
-        let (_, puzzle_commitment) = puzzle_solution.get_solution_commitment::<sha2::Sha256>();
-
-        let wrong_solutions = vec![
-            "Solution1".to_string(),
-            "solution2".to_string(),
-            "solution3".to_string(),
-        ];
-        let wrong_puzzle_solution =
-            PuzzleSolution::<ark_bn254::G1Projective>::new(wrong_solutions, g);
-        let (wrong_w, _) = wrong_puzzle_solution.get_solution_commitment::<sha2::Sha256>();
-
-        assert!(puzzle_solution
-            .get_solution_proof::<sha2::Sha256>(wrong_w, puzzle_commitment)
-            .is_err());
-    }
-
     // Test for verify_and_export
-    #[test]
+    #[test] 
     fn test_verify_and_export() {
         let solutions = vec![
             "solution1".to_string(),
             "solution2".to_string(),
             "solution3".to_string(),
         ];
-        let g = ark_bn254::G1Projective::generator();
-        let puzzle_solution = PuzzleSolution::<ark_bn254::G1Projective>::new(solutions, g);
-        let (w, puzzle_commitment) = puzzle_solution.get_solution_commitment::<sha2::Sha256>();
+        let puzzle_solution = PuzzleSolution::new(solutions);
+        let (w, puzzle_commitment) = puzzle_solution.get_solution_commitment::<sha2::Sha256>().unwrap();
+        let m_s = LocalWallet::new(&mut rand::thread_rng()).address().as_bytes().to_vec();
         let puzzle_solution_proof = puzzle_solution
-            .get_solution_proof::<sha2::Sha256>(w, puzzle_commitment)
+            .get_solution_proof(w.to_bytes().to_vec(), puzzle_commitment, m_s)
             .unwrap();
 
         let (
-            ax_bytes,
-            ay_bytes,
-            gx_bytes,
-            gy_bytes,
-            solution_commitment_x_bytes,
-            solution_commitment_y_bytes,
-            z_bytes
+            a_bytes,
+            m_s_bytes,
         ) = puzzle_solution_proof
-            .verify_and_export::<sha2::Sha256>(g, puzzle_commitment)
+            .verify_and_export(puzzle_commitment)
             .unwrap();
 
-        assert!(z_bytes.len() > 0);
-        assert!(ax_bytes.len() > 0);
-        assert!(gx_bytes.len() > 0);
-        assert!(solution_commitment_x_bytes.len() > 0);
-        assert!(ay_bytes.len() > 0);
-        assert!(gy_bytes.len() > 0);
-        assert!(solution_commitment_y_bytes.len() > 0);
+        assert!(a_bytes.len() > 0);
+        assert!(m_s_bytes.len() > 0);
     }
 }
