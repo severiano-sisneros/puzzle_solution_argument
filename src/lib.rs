61f180a5e95807040752667f17962515d54121c7
Severiano Sisneros <seve.sisneros@protonmail.com>
2023-08-21 19:04:51 -0700

    BREAKING CHANGE: Switched to using ECDSA for proving

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 
 
-use ethers_core::k256::ecdsa::{SigningKey, VerifyingKey, Signature, signature::{Signer, Verifier}};
+use ethers_core::abi::AbiEncode;
+use ethers_core::types::{Signature, Address, H256};
+use ethers_core::utils::keccak256;
+use ethers_signers::{Signer, Wallet};
 use digest::Digest;
 use anyhow::Result;
 
@@ -14,7 +17,7 @@ pub struct PuzzleSolution {
 /// The proof is essentially a Schnorr proof of knowledge of the discrete logarithm of a commitment to the solution.
 pub struct PuzzleSolutionProof {
     a: Signature,
-    m_s: Vec<u8>,
+    m_s: Address,
 }
 
 impl PuzzleSolution{
@@ -23,7 +26,7 @@ impl PuzzleSolution{
     }
 
     /// get_solution_commitment returns the commitment to the solution set.
-    pub fn get_solution_commitment<H: Digest>(&self) -> Result<(SigningKey, VerifyingKey)>  {
+    pub fn get_solution_commitment<H: Digest>(&self) -> Result<( Wallet<ecdsa::SigningKey<k256::Secp256k1>>, Address)>  {
         // Compute chain of hashes of each solution in the solution set
         // and commit to the last hash in the chain
         let mut hasher = H::new();
@@ -34,30 +37,32 @@ impl PuzzleSolution{
 
         let w_bytes = hasher.finalize();
 
-        let w = SigningKey::from_slice(&w_bytes)?;
+        let w: Wallet<ecdsa::SigningKey<k256::Secp256k1>> = Wallet::from_bytes(&w_bytes)?;
         let binding = w.clone();
-        let h = binding.verifying_key();
-        Ok((w, *h))
+        let h = binding.address();
+        Ok((w, h))
     }
 
     /// get_solution_proof returns a proof that the given solution is a valid solution to the puzzle.
-    pub fn get_solution_proof(
+    pub async fn get_solution_proof(
         &self,
-        w_bytes: Vec<u8>,
-        solution_commitment: VerifyingKey,
-        m_s: Vec<u8>,
+        w: Wallet<ecdsa::SigningKey<k256::Secp256k1>>,
+        solution_commitment: Address,
+        m_s: Address,
     ) -> Result<PuzzleSolutionProof> {
         // Check that w is correct solution
         //TODO: Better error handling
-        let w = SigningKey::from_slice(&w_bytes)?;
-        let h = w.verifying_key();
-        match *h == solution_commitment {
+        let h = w.address();
+        match h == solution_commitment {
             true => (),
             false => return Err(anyhow::Error::msg("Invalid solution")),
         }
 
-        // Send blinding commitment
-        let a = w.sign(m_s.as_slice());
+        // Compute hash of m_s
+        let m_s_hash = H256::from(keccak256(m_s.encode()));
+
+        // Sign hash of m_s
+        let a = w.sign_hash(m_s_hash)?;
 
         Ok(PuzzleSolutionProof{ a, m_s})
     }
@@ -67,29 +72,38 @@ impl PuzzleSolutionProof {
     /// verify returns true if the proof is valid and false otherwise.
     pub fn verify<H: Digest>(
         &self,
-        solution_commitment: VerifyingKey,
+        solution_commitment: Address,
     ) -> Result<bool> {
 
         // Verify proof
-        let result = solution_commitment.verify(self.m_s.as_slice(), &self.a);
-        Ok(result.is_ok())
+        let m_s_hash = H256::from(keccak256(self.m_s.encode()));
+        let result = self.a.recover(m_s_hash)?;
+        Ok(result==solution_commitment)
     }
 
     /// verify returns serialized bytes for self.a, self.z, g, and solution_commitment if the proof is valid and error otherwise.
     pub fn verify_and_export(
         &self,
-        solution_commitment: VerifyingKey,
-    ) -> Result<(Vec<u8>, Vec<u8>   )> {
+        solution_commitment: Address,
+    ) -> Result<(Vec<u8>, Vec<u8>, Vec<u8>, u8, Vec<u8> )> {
   
         // Verify proof
-        let result = solution_commitment.verify(&self.m_s.as_slice(), &self.a);
-        match result.is_ok() {
+        let m_s_bytes = self.m_s.clone();
+        let m_s_hash = H256::from(keccak256(self.m_s.clone().encode()));
+        let result = self.a.recover(m_s_hash)?;
+        match result == solution_commitment {
             true => {
                 let a_bytes = self.a.to_vec();
+                let r_abi = self.a.r.encode();
+                let s_abi = self.a.s.encode();
+                let v_abi = self.a.recovery_id()?.to_byte();
 
                 Ok((
                     a_bytes,
-                    self.m_s.clone()
+                    r_abi,
+                    s_abi,
+                    v_abi,
+                    m_s_bytes.as_bytes().to_vec(),
                 ))
             }
             false => Err(anyhow::Error::msg("Invalid proof")),
@@ -104,9 +118,10 @@ mod tests {
     use super::*;
     use sha2;
     use ethers_signers::{Signer, LocalWallet};
+    use tokio;
 
-    #[test]
-    fn test_get_solution_commitment() {
+    #[tokio::test]
+    async fn test_get_solution_commitment() {
         let solutions = vec![
             "solution1".to_string(),
             "solution2".to_string(),
@@ -114,23 +129,33 @@ mod tests {
         ];
         let puzzle_solution = PuzzleSolution::new(solutions);
         let (w, puzzle_commitment) = puzzle_solution.get_solution_commitment::<sha2::Sha256>().unwrap();
-
-        assert!(*w.verifying_key() == puzzle_commitment);
-    }
-    // Test for get_solution_proof and verify
-    #[test]
-    fn test_get_solution_proof() {
-        let solutions = vec![
-            "solution1".to_string(),
-            "solution2".to_string(),
-            "solution3".to_string(),
-        ];
-        let puzzle_solution = PuzzleSolution::new(solutions);
-        let (w, puzzle_commitment) = puzzle_solution.get_solution_commitment::<sha2::Sha256>().unwrap();
-        let m_s = LocalWallet::new(&mut rand::thread_rng()).address().as_bytes().to_vec();
+        let m_s = LocalWallet::new(&mut rand::thread_rng()).address();
         let puzzle_solution_proof = puzzle_solution
-            .get_solution_proof(w.to_bytes().to_vec(), puzzle_commitment, m_s.clone())
-            .unwrap();
+            .get_solution_proof(w, puzzle_commitment, m_s)
+            .await.unwrap();
+
+        assert!(
+            puzzle_solution_proof
+                .verify::<sha2::Sha256>(puzzle_commitment)
+                .unwrap()
+                == true
+        );
+    }
+
+    // Test for get_solution_proof and verify
+    #[tokio::test]
+    async fn test_get_solution_proof() {
+        let solutions = vec![
+            "solution1".to_string(),
+            "solution2".to_string(),
+            "solution3".to_string(),
+        ];
+        let puzzle_solution = PuzzleSolution::new(solutions);
+        let (w, puzzle_commitment) = puzzle_solution.get_solution_commitment::<sha2::Sha256>().unwrap();
+        let m_s = LocalWallet::new(&mut rand::thread_rng()).address();
+        let puzzle_solution_proof = puzzle_solution
+            .get_solution_proof(w, puzzle_commitment, m_s.clone())
+            .await.unwrap();
 
         assert!(
             puzzle_solution_proof
@@ -149,8 +174,8 @@ mod tests {
         let (wrong_w, wrong_puzzle_commitment) =
             wrong_puzzle_solution.get_solution_commitment::<sha2::Sha256>().unwrap();
         let wrong_puzzle_solution_proof = wrong_puzzle_solution
-            .get_solution_proof(wrong_w.to_bytes().to_vec(), wrong_puzzle_commitment, m_s)
-            .unwrap();
+            .get_solution_proof(wrong_w, wrong_puzzle_commitment, m_s)
+            .await.unwrap();
 
         assert!(
             wrong_puzzle_solution_proof
@@ -161,8 +186,8 @@ mod tests {
     }
 
     // Test for verify_and_export
-    #[test] 
-    fn test_verify_and_export() {
+    #[tokio::test]
+    async fn test_verify_and_export() {
         let solutions = vec![
             "solution1".to_string(),
             "solution2".to_string(),
@@ -170,19 +195,25 @@ mod tests {
         ];
         let puzzle_solution = PuzzleSolution::new(solutions);
         let (w, puzzle_commitment) = puzzle_solution.get_solution_commitment::<sha2::Sha256>().unwrap();
-        let m_s = LocalWallet::new(&mut rand::thread_rng()).address().as_bytes().to_vec();
+        let m_s = LocalWallet::new(&mut rand::thread_rng()).address();
         let puzzle_solution_proof = puzzle_solution
-            .get_solution_proof(w.to_bytes().to_vec(), puzzle_commitment, m_s)
-            .unwrap();
+            .get_solution_proof(w, puzzle_commitment, m_s.clone())
+            .await.unwrap();
 
         let (
             a_bytes,
+            r_abi,
+            s_abi,
+            v_abi,
             m_s_bytes,
         ) = puzzle_solution_proof
             .verify_and_export(puzzle_commitment)
             .unwrap();
 
-        assert!(a_bytes.len() > 0);
-        assert!(m_s_bytes.len() > 0);
+        assert!(a_bytes.len() == 65);
+        assert!(r_abi.len() == 32);
+        assert!(s_abi.len() == 32);
+        assert!(v_abi == 0 || v_abi == 1 );
+        assert!(m_s_bytes.len() == 20);
     }
 }
